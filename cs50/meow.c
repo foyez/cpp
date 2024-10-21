64bebc1894ad52e3bdc1f97d7826eaf98e8efb00
Kazi Foyez Ahmed <foyezar@gmail.com>
2024-10-21 20:32:16 +0200

    loops

---
+++ cs50/meow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i = 3;
+
+  while (i > 0)
+  {
+    printf("meow\n");
+    i--;
+  }
+
+  for (int i = 0; i < 3; i++)
+  {
+    printf("meow\n");
+  }
+}
