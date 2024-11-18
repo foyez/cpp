3ba73d5c8b60b704aeb7606f765a3b9284622db2
Kazi Foyez Ahmed <foyezar@gmail.com>
2024-11-18 13:53:43 +0100

    solve strcmp and strrev

---
+++ 42/exam/exam00/ft_strcmp.c
@@ -0,0 +1,21 @@
+int ft_strcmp(char *s1, char *s2)
+{
+  int i = 0;
+
+  i = 0;
+  while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+  {
+    i++;
+  }
+  return (s1[i] - s2[i]);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+  char s1[] = "hi";
+  char s2[] = "hello";
+  printf("%d\n", ft_strcmp(s1, s2));
+  return (0);
+}
---
+++ 42/exam/exam00/ft_strrev.c
@@ -0,0 +1,38 @@
+int ft_strlen(char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[i] != '\0')
+  {
+    i++;
+  }
+  return (i);
+}
+
+char *ft_strrev(char *str)
+{
+  int i;
+  int size;
+  char tmp;
+
+  i = 0;
+  size = ft_strlen(str);
+  while (i < size / 2)
+  {
+    tmp = str[i];
+    str[i] = str[size - 1 - i];
+    str[size - 1 - i] = tmp;
+    i++;
+  }
+  return (str);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+  char str[] = "hello";
+  printf("%s\n", ft_strrev(str));
+  return (0);
+}
