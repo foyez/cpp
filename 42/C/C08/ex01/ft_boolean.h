906387ea30528be585be605dbe0f93267a3b9809
Kazi Foyez Ahmed <foyezar@gmail.com>
2024-11-28 20:23:55 +0100

    add ex00-ex01 of C08

---
+++ 42/C/C08/ex01/ft_boolean.h
@@ -0,0 +1,17 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum e_bool
+{
+    FALSE = 0,
+    TRUE = 1
+}   t_bool;
+
+# define SUCCESS 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+#endif
