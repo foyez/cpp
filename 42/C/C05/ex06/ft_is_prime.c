183bbe20e3b84e2016f95aec55e42714d0cd2bd8
Kazi Foyez Ahmed <foyezar@gmail.com>
2024-12-04 16:37:57 +0100

    C09 ex00

--- 42/C/C05/ex06/ft_is_prime.c
+++ 42/C/C05/ex06/ft_is_prime.c
@@ -3,17 +3,17 @@ int	ft_is_prime(int nb)
 	int	i;
 
 	if (nb <= 1)
-		return (0); // Numbers <= 1 are not prime
+		return (0);
 	if (nb <= 3)
-		return (1); // 2 and 3 are prime numbers
+		return (1);
 	if (nb % 2 == 0 || nb % 3 == 0)
-		return (0); // Eliminate multiples of 2 and 3
+		return (0);
 	i = 5;
 	while (i * i <= nb)
 	{
 		if (nb % i == 0 || nb % (i + 2) == 0)
-			return (0); // Eliminate factors
-		i += 6;         // Check numbers of the form 6k ± 1
+			return (0);
+		i += 6;
 	}
-	return (1); // Prime if no factors are found
+	return (1);
 }
