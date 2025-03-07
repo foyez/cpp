1b463025fb49eb9a6e3be1951c0ebe45e4ccb0bd
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-03-07 21:53:57 +0100

    add piscine shell projects

--- 42/kickoff/ft_printf/include/ft_printf.h
+++ 42/kickoff/ft_printf/include/ft_printf.h
@@ -1,5 +1,6 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdlib.h>
 
 #endif
--- 42/kickoff/ft_printf/src/ft_printf.c
+++ 42/kickoff/ft_printf/src/ft_printf.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include "printf.h"
 
-int ft_printf()
+int ft_printf(const char *s, ...)
 {
+	if (s == NULL)
+		return 0;
 	return 1;
 }
