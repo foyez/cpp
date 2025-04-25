959485e3e36c531b2476a6d2c06b84c79496b1aa
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-04-25 15:23:26 +0200

    fix header issue

--- 42/kickoff/ft_printf_bonus_strict/include/ft_printf.h
+++ 42/kickoff/ft_printf_bonus_strict/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kaahmed <kaahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 20:16:00 by kaahmed           #+#    #+#             */
-/*   Updated: 2025/04/25 01:37:23 by kaahmed          ###   ########.fr       */
+/*   Updated: 2025/04/25 15:23:05 by kaahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ typedef struct s_vars
 typedef struct s_hex_args
 {
 	int		low;
-	int		prefixlen
+	int		prefixlen;
 }			t_hex_args;
 
 int			ft_printf(const char *str, ...);
