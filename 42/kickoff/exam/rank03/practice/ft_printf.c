adff72e9aa73a94b4c40b7e7cadef6513535d3f3
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-06-26 15:53:16 +0200

    add practice for exam 03

---
+++ 42/kickoff/exam/rank03/practice/ft_printf.c
@@ -0,0 +1,87 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int print_str(char *s)
+{
+	int i = -1;
+
+	if (!s)
+		s = "(null)";
+	while (s[++i])
+		ft_putchar(s[i]);
+	return (i);
+}
+
+int print_num(int n)
+{
+	int count = 0;
+
+	if (n == -2147483648)
+		return (print_str("-2147483648"));
+	if (n < 0)
+	{
+		count += ft_putchar('-');
+		count += print_num(-n);
+	}
+	else
+	{
+		if (n > 9)
+			count += print_num(n / 10);
+		count += ft_putchar(n % 10 + '0');
+	}
+	return (count);
+}
+
+int print_hex(unsigned int n)
+{
+	int count = 0;
+	char *base = "0123456789abcdef";
+
+	if (n > 15)
+		count += print_hex(n / 16);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
+
+int handle_format(char c, va_list *args)
+{
+	if (c == 's')
+		return (print_str(va_arg(*args, char *)));
+	if (c == 'd')
+		return (print_num(va_arg(*args, int)));
+	if (c == 'x')
+		return (print_hex(va_arg(*args, unsigned int)));
+	return (ft_putchar(c));
+}
+
+int ft_printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+		{
+			format++;
+			count += handle_format(*format, &args);
+		}
+		else
+			count += ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
+
+int main(void)
+{
+	ft_printf("Hello %s, your score is %d, hex: %x\n", "Foyez", 100, 100);
+	return (0);
+}
