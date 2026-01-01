dc61da2cad0ae84085764c3d3f4ea8893e675a8a
Kazi Foyez Ahmed <foyezar@gmail.com>
2026-01-01 16:48:01 +0100

    add copy contructor and assignment operator

--- 42/kickoff/exam/rank05/vect2/vect2.cpp
+++ 42/kickoff/exam/rank05/vect2/vect2.cpp
@@ -3,6 +3,23 @@
 // default constructor - initialize to (0, 0)
 vect2::vect2() : x(0), y(0) {}
 
+// parameterized constructor
+vect2::vect2(int x, int y) : x(x), y(y) {}
+
+// copy constructor
+vect2::vect2(const vect2 &other) : x(other.x), y(other.y) {}
+
+// assignment operator
+vect2 &vect2::operator=(const vect2 &other)
+{
+  if (this != &other)
+  {
+    x = other.x;
+    y = other.y;
+  }
+  return *this;
+}
+
 int &vect2::operator[](int index)
 {
   if (index == 0)
--- 42/kickoff/exam/rank05/vect2/vect2.hpp
+++ 42/kickoff/exam/rank05/vect2/vect2.hpp
@@ -11,7 +11,12 @@ private:
 
 public:
   // constructor
-  vect2(); // default constructor
+  vect2();                   // default constructor
+  vect2(int x, int y);       // parameterized constructor
+  vect2(const vect2 &other); // copy constructor
+
+  // assignment operator
+  vect2 &operator=(const vect2 &other);
 
   // array subscript operator
   int &operator[](int index); // non-const version
